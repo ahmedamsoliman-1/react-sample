c3a8742b98ed441a1f9ab64799028fefed185882
Ahmed Soliman <ahmed.soliman@avrioc.com>
2023-12-17 16:46:23 +0400

    Updated in 'Khalifah A City, AE' on BRANCH 'session-1' BY 'ahmed.soliman' ON 'AVRAST3412' for the files:  package-lock.json package.json src/App.js update_git.sh - Before

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [token, setToken] = useState('');
+  const [userData, setUserData] = useState(null);
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://localhost:5000/login', {
+        username,
+        password,
+      });
+      setToken(response.data.token);
+    } catch (error) {
+      console.error('Login error:', error);
+    }
+  };
+
+  const fetchUserData = async (token) => {
+    try {
+      const response = await axios.get('http://localhost:5000/data', {
+        headers: {
+          Authorization: token,
+        },
+      });
+      setUserData(response.data);
+    } catch (error) {
+      console.error('Fetch data error:', error);
+    }
+  };
+
+  const handleLogout = () => {
+    setToken('');
+    setUserData(null);
+  };
+
+  useEffect(() => {
+    if (token) {
+      fetchUserData(token);
+    }
+  }, [token]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!token ? (
+        <div>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handleLogin}>Login</button>
+        </div>
+      ) : (
+        <div>
+          <p>Logged in as: {userData && userData.username}</p>
+          <p>Sensitive Data: {userData && userData.sensitiveData}</p>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      )}
     </div>
   );
 }
--- update_git.sh
+++ update_git.sh
@@ -3,18 +3,25 @@
 city=$(curl -s ipinfo.io/city)
 country=$(curl -s ipinfo.io/country)
 
-
 # Get the name of the current branch
 branch=$(git symbolic-ref --short HEAD)
 
 # Get the list of modified or added files
 modified_files=$(git status --porcelain | awk '{if ($1 == "M" || $1 == "A") print $2}')
 
-# Commit message mentioning the updated or added files and the branch
+# Optional message provided as an argument, defaulting to an empty string if not provided
+optional_message="${1:-}"
+
+# Commit message mentioning the updated or added files and the branch, including the optional message if provided
 commit_message="Updated in '$city, $country' on BRANCH '$branch' BY '$(whoami)' ON '$(hostname)' for the files: "
 for file in $modified_files; do
   commit_message+=" $file"
 done
+
+# Append the optional message to the commit message if it's not empty
+if [ -n "$optional_message" ]; then
+  commit_message+=" - $optional_message"
+fi
 
 git config --global user.name "Ahmed Soliman"
 git config --global user.email "ahmed.soliman@avrioc.com"
@@ -23,6 +30,5 @@ git config --global user.email "ahmed.soliman@avrioc.com"
 git add .
 git commit -m "$commit_message"
 git push origin $branch
-
 
 sleep 3s
